bbfdc16411ad34439dc741d5f9ea6161e09296b3
bergholmm <marcusbergholm@hotmail.com>
2020-01-31 14:58:05 +0100

    Update install scripts

--- install_linux.bash
+++ install_linux.bash
@@ -1,23 +1,34 @@
-# WIP: needs to be updated for Arch-linux
-sudo pacman -S fish
+echo "Installing fish"
+yay -S fish
 chsh -s `which fish`
 
-sudo pacman -S ripgrep
-sudo pacman -S neovim
+echo "Installing packages"
+yay -S ripgrep fzf neovim yarn npm nvm brave ranger
 
-pip3 install --user pynvim
-git clone https://github.com/VundleVim/Vundle.vim.git ./config/nvim/bundle/Vundle.vim
-
+echo "Linking files"
 fish ./linkfiles.fish
+
+echo "Setting up neovim"
+git clone https://github.com/VundleVim/Vundle.vim.git ./config/nvim/bundle/Vundle.vim
+pip3 install --user pynvim
 
 nvim +PluginInstall +qall
 nvim +UpdateRemotePlugins +qall
+
 cd ./config/nvim/bundle/vimproc.vim
 make
 cd -
+cd ./config/nvim/bundle/coc.nvim
+./install.sh nightly
+cd -
 
+nvim +CocInstall coc-tsserver coc-eslint coc-json coc-prettier coc-css +qall
+
+echo "Setting up fish"
 curl -L https://get.oh-my.fish | fish
 omf install https://github.com/jhillyerd/plugin-git
 omf install bass
 omf install pure
+
 ln -s $OMF_PATH/themes/pure/conf.d/pure.fish ~/.config/fish/conf.d/pure.fish
+ln -s $OMF_PATH/themes/pure/conf.d/_pure_init.fish ~/.config/fish/conf.d/pure_init.fish
--- install_macos.bash
+++ install_macos.bash
@@ -11,48 +11,46 @@ brew install fish
 echo "/usr/local/bin/fish" | sudo tee -a /etc/shells
 chsh -s /usr/local/bin/fish
 
-echo "Installing iTerm2"
+echo "Installing packages"
 brew tap caskroom/versions
 brew cask install iterm2-nightly
-
-echo "Installing yarn"
 brew install yarn
-
-echo "Installing ripgrep + fzf"
 brew install ripgrep
 brew install fzf
-
-echo "Setting up Neovim"
+brew install ranger
 brew install python
 brew install python2
 brew install neovim
-git clone https://github.com/VundleVim/Vundle.vim.git ./config/nvim/bundle/Vundle.vim
+brew cask install brave
 
+echo "Linking files"
 fish ./linkfiles.fish
 
+echo "Setting up Neovim"
+git clone https://github.com/VundleVim/Vundle.vim.git ./config/nvim/bundle/Vundle.vim
 pip3 install --user pynvim
 
 nvim +PluginInstall +qall
 nvim +UpdateRemotePlugins +qall
+
 cd ./config/nvim/bundle/vimproc.vim
 make
 cd -
 cd ./config/nvim/bundle/coc.nvim
 ./install.sh nightly
 cd -
+
 nvim +CocInstall coc-tsserver coc-eslint coc-json coc-prettier coc-css +qall
 
-echo "Installing Brave"
-brew cask install brave
-
-# Remove last login when starting new terminal
-touch ~/.hushlogin
-
-echo "Setting up omf"
+echo "Setting up fish"
 curl -L https://get.oh-my.fish | fish
 omf install https://github.com/jhillyerd/plugin-git
 omf install bass
 omf install pure
 ln -s $OMF_PATH/themes/pure/conf.d/pure.fish ~/.config/fish/conf.d/pure.fish
+ln -s $OMF_PATH/themes/pure/conf.d/_pure_init.fish ~/.config/fish/conf.d/pure_init.fish
 
 echo "Setup iTerm2 tab-bar/tabs: https://www.felixjung.io/posts/pretty-iterm2-with-a-modern-titlebar/"
+
+# Remove last login when starting new terminal
+touch ~/.hushlogin

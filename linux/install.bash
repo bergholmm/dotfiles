04df98e55c6a52286338904cd6977506606bccf6
Bergholmm <berghommarcus@gmail.com>
2021-06-13 20:14:40 +0200

    update linux install script

--- linux/install.bash
+++ linux/install.bash
@@ -2,4 +2,35 @@
 
 set -e
 
-echo "Installing"
+echo "Updating system"
+yay
+
+echo "Installing packages"
+yay -S fish yarn ripgrep fzf neovim python-neovimc direnv-bin firefox-developer-edition insomnia-bin slack-desktop visual-studio-code-bin ttf-fira-code
+
+echo "Setting fish as default"
+chsh -s `which fish`
+
+fish ~/dotfiles/linkfiles.fish
+
+echo "Installing nvm"
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+
+echo "Setting up neovim"
+git clone https://github.com/VundleVim/Vundle.vim.git ~/dotfiles/nvim/bundle/Vundle.vim
+
+nvim +PluginInstall +qall
+nvim +UpdateRemotePlugins +qall
+
+cd ~/dotfiles/nvim/bundle/vimproc.vim
+make
+cd ~/dotfiles/nvim/bundle/coc.nvim
+yarn install --frozen-lockfile
+cd ~/dotfiles/nvim/bundle/LeaderF
+./install.sh
+
+nvim -c 'CocInstall coc-tsserver coc-eslint coc-json coc-prettier coc-css'
+
+echo "Setting up fish"
+echo "run ./install_packages.fish to install fish packages"
+curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher

89d9924b2e23f26ac086948cd66d308375df8d11
Marcus Bergholm <bergholm@Sagas-MBP.lan>
2021-12-30 14:44:37 +0100

    Embark colorscheme for fish

--- fish/config.fish
+++ fish/config.fish
@@ -46,3 +46,31 @@ set PATH $HOME/go/bin $PATH
 
 # Setup direnv
 direnv hook fish | source
+
+
+# Embark colorscheme for fish shell
+# set fish_color_autosuggestion FFE6B3
+# set fish_color_command A1EFD3
+# set fish_color_comment 585273
+# set fish_color_cwd FFE6B3
+# set fish_color_end D4BFFF
+# set fish_color_error F02E6E
+# set fish_color_escape F2B482
+# set fish_color_keyword F48FB1
+# set fish_color_match $fish_color_cwd
+# set fish_color_normal CBE3E7
+# set fish_color_operator --bold $fish_color_cwd
+# set fish_color_param $fish_color_normal
+# set fish_color_quote 87DFEB
+# set fish_color_redirection F48FB1
+# set fish_color_search_match --background=3E3859
+# set fish_color_selection $fish_color_comment
+#
+# set fish_color_cancel 3E3859
+# set fish_color_host 63F2F1
+# set fish_color_host_remote $fish_color_quote
+# set fish_color_user 62D196
+#
+# set fish_pager_color_progress --background=63F2F1 1E1C31
+# set fish_pager_color_description $fish_color_command
+# set fish_pager_color_selected_description --underline $fish_color_command

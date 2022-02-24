743da4168d7234de6a4be46a045a0bd5ed8ed5ca
Marcus Bergholm <marcus.bergholm@embark-studios.com>
2022-02-24 10:41:37 +0100

    Add windows folder

---
+++ windows/install.ps1
@@ -0,0 +1,20 @@
+# Install pwsh: https://github.com/PowerShell/PowerShell
+# Install choco: https://chocolatey.org/install#individual
+
+# choco install neovim mingw ripgrep nodejs python
+# firefox-developer-edition insomnia-bin slack-desktop visual-studio-code-bin
+# nerd-fonts
+
+# Install nvchad
+# git clone https://github.com/wbthomason/packer.nvim "$env:LOCALAPPDATA\nvim-data\site\pack\packer\start\packer.nvim"
+# git clone https://github.com/NvChad/NvChad nvim --depth 1 --> Move to ~/appdata/local/nvim
+# Move custom config from dotfiles
+
+# Install rust: https://www.rust-lang.org/tools/install
+# Install prompt: cargo install starship --locked
+
+# Move powershell config to: C:\Users\marcus.bergholm\Documents\PowerShell\Microsoft.PowerShell_profile.ps
+# Install git aliases: https://github.com/gluons/powershell-git-aliases
+
+# npm i -g yarn eslint prettier neovim
+# pip install pynvim

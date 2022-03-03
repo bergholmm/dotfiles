8297a6bb27a5b97d1fd2d91888352b7971df2f67
Bergholmm <marcus.bergholm@embark-studios.com>
2022-03-03 15:49:04 +0100

    use setup

--- nvim/lua/custom/plugins/init.lua
+++ nvim/lua/custom/plugins/init.lua
@@ -45,5 +45,12 @@ return {
   },
   {
     "ahmedkhalf/project.nvim",
+    config = function()
+      require("project_nvim").setup {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      }
+    end,
   },
 }

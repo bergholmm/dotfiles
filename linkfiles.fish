c66c4e9486e356389bac76e9104190453f57b7c8
Marcus Bergholm <marcus@kive.ai>
2022-09-27 13:57:53 +0200

    Add yabai and skhd

--- linkfiles.fish
+++ linkfiles.fish
@@ -18,3 +18,11 @@ end
 mv ~/.tmux.conf $olddir/;
 echo "Creating symlink: ~/.tmux.conf -> $dir/tmux/.tmux.conf"
 ln -sf $dir/tmux/.tmux.conf ~/.tmux.conf
+
+mv ~/.yabairc $olddir/;
+echo "Creating symlink: ~/.yabairc -> $dir/yabai/.yabairc"
+ln -sf $dir/yabai/.yabairc ~/.yabairc
+
+mv ~/.skhdrc $olddir/;
+echo "Creating symlink: ~/.skhdrc -> $dir/skhd/.skhdrc"
+ln -sf $dir/skhd/.skhdrc ~/.skhdrc
--- nvim/lua/user/lsp/null-ls.lua
+++ nvim/lua/user/lsp/null-ls.lua
@@ -14,7 +14,7 @@ null_ls.setup({
     formatting.black.with({ extra_args = { '--fast' } }),
     formatting.stylua,
     formatting.gofumpt,
-    diagnostics.eslint,
+    diagnostics.eslint_d,
   },
   on_attach = function(client, bufnr)
     if client.supports_method('textDocument/formatting') then

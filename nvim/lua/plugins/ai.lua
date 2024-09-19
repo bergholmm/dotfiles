36945ef064cda3b2cd58a1528e90b0a62d07f63a
Bergholmm <marcus.bergholm@embark-studios.com>
2024-09-19 14:43:38 +0200

    Remove copilot from cmp

---
+++ nvim/lua/plugins/ai.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    build = ":Copilot auth",
+    event = "InsertEnter",
+    opts = {
+      suggestion = { enabled = true, auto_trigger = true, keymap = { accept = "<C-l>" } },
+      panel = {
+        enabled = true,
+        auto_refresh = false,
+        keymap = {
+          jump_prev = "[[",
+          jump_next = "]]",
+          accept = "<CR>",
+          refresh = "gr",
+          open = "<M-CR>",
+        },
+        layout = {
+          position = "bottom", -- | top | left | right
+          ratio = 0.4,
+        },
+      },
+      filetypes = {
+        markdown = true,
+        help = true,
+      },
+    },
+  },
+}
--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -7,4 +7,8 @@ return {
     "folke/flash.nvim",
     enabled = false,
   },
+  {
+    "zbirenbaum/copilot-cmp",
+    endabled = false,
+  },
 }
